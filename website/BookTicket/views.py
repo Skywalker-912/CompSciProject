d3cf02bc61ac5ad5d88feca0531f1cc60b8aa054
Ankith Praveen <C18BS75819@SKS.LOCAL>
2019-10-17 11:34:31 +0530

    update

--- website/BookTicket/views.py
+++ website/BookTicket/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 import csv
 from BookTicket.models import Account
 import os
-
+import mysql.connector
+con=mysql.connector.connect(host="localhost", user="root", passwd="root",database="project")
 # Create your views here.
+a_list=[]
+email=''
+pwd=''
 def seehome(request):
-    return render(request,'Home_Page.html')
+    return render(request,'Home_Page.html',{'al':[]})
 def seelogin(request):
+    global a_list
+    global email
+    global pwd
     if request.method=="POST":
         email=request.POST['email']
         pwd=request.POST['password']
@@ -15,13 +22,33 @@ def seelogin(request):
         for i in range(len(a_list)):
             print(a_list)
             if email==a_list[i].aemail and pwd==a_list[i].apwd:
-               return render(request,'Registered_Home.html',{'al':a_list[i:i+1]})
+                #request.session['a_list']=a_list[i:i+1]
+                return render(request,'Home_Page.html',{'al':a_list[i:i+1]})
+            else:
+                return render(request,'Login.html')
     else:
-        return render(request,'Login.html')
+        return render(request,'Login.html',{'al':[]})
 def seepnr(request):
-    return render(request,'PNR status.html')
+    #x=request.session['a_list']
+    global a_list
+    global email
+    global pwd
+    a=Account.objects.all()
+    for i in range(len(a_list)):
+            print(a_list)
+            if email==a_list[i].aemail and pwd==a_list[i].apwd:
+                #request.session['a_list']=a_list[i:i+1]
+                return render(request,'PNR status.html',{'al':a_list[i:i+1]})
 def seesearch(request):
-    return render(request,'Search.html')
+    global a_list
+    global email
+    global pwd
+    a=Account.objects.all()
+    for i in range(len(a_list)):
+            print(a_list)
+            if email==a_list[i].aemail and pwd==a_list[i].apwd:
+                #request.session['a_list']=a_list[i:i+1]
+                return render(request,'PNR status.html',{'al':a_list[i:i+1]})
 def seereg(request):
     if request.method=="POST":
         name=request.POST['name']
@@ -47,7 +74,11 @@ def seereg(request):
         print(a_list)    
         k=a_list[-1:]
         print(k)
-        return render(request,'Registered_Home.html',{'al':k}) 
-        print(al)
+        return render(request,'Home_Page.html',{'al':k}) 
     else:
         return render(request,'Register.html')
+def seeschedule(request):
+    curs=con.cursor()
+    curs.execute('Select * from bookticket_train')
+    train=curs.fetchall()
+    return render(request,'Schedule.html',{'train':train})
