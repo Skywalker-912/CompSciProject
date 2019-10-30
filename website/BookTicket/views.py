16adea9975a69748c1f3b5d34dd885cc9ab7f42b
Ankith Praveen <C18BS75819@SKS.LOCAL>
2019-10-30 08:57:09 +0530

    update

--- website/BookTicket/views.py
+++ website/BookTicket/views.py
@@ -17,7 +17,8 @@ x=[]
 train=[]
 pnrlist=[]
 tno=''
-day1=''
+fdate=''
+pnr=0
 datedict={'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
 daydict={1:'MON',2:'TUE',3:'WED',4:'THU',5:'FRI',6:'SAT',7:'SUN'}
 
@@ -33,17 +34,31 @@ def seelogin(request):
     if request.method=="POST":
         email=request.POST['email']
         pwd=request.POST['password']
-        a=Account.objects.all()
-        a_list=list(a)
-        for i in range(len(a_list)):
-            print(a_list)
-            if email==a_list[i].aemail and pwd==a_list[i].apwd:
-                x=a_list[i:i+1]
-                return render(request,'Home_Page.html',{'al':x})
-            else:
-                return render(request,'Login.html')
+        print(email,pwd)
+        curs.execute("select * from bookticket_account")
+        acc=curs.fetchall()
+        print(acc)
+        for i in acc:
+            print(i)
+            if i[2]==email and i[3]==pwd:
+                x=i
+                print(x)
+                return render(request,'Home_Page.html',{'al':i})
     else:
         return render(request,'Login.html',{'al':[]})
+        # email=request.POST['email']
+        # pwd=request.POST['password']
+        # a=Account.objects.all()
+        # a_list=list(a)
+        # for i in range(len(a_list)):
+            # print(i)
+            # if email==a_list[i].aemail and pwd==a_list[i].apwd:
+                # x=a_list[i:i+1]
+                # return render(request,'Home_Page.html',{'al':x})
+            # else:
+                # return render(request,'Login.html')
+    # else:
+    #     return render(request,'Login.html',{'al':[]})
 def seepnr(request):
     global x
     return render(request,'PNR status.html',{'al':x})
@@ -51,7 +66,7 @@ def seesearch(request):
     global train
     global x
     global datedict
-    global day1
+    global fdate
     if request.method=="POST":
         fromstat=request.POST['fromstat']
         tostat=request.POST['tostat']
@@ -62,6 +77,7 @@ def seesearch(request):
         year,month,dat=datesplit
         fdate=datetime.datetime(int(year),int(month),int(dat))
         day=fdate.isoweekday()
+        print(fdate,'bye')
         # month=datedict[date[0:3]]
         # year=date[8:]
         # dat=date[4:6]
@@ -107,14 +123,14 @@ def seereg(request):
         name=request.POST['name']
         email=request.POST['email']
         pwd=request.POST['password']
-        repwd=request.POST['repassword']
+        # repwd=request.POST['repassword']
         age=request.POST['age']
         with open('data.csv','a') as file:
             wcs=csv.writer(file)
             wcs.writerow(["name",name])
             wcs.writerow(["email",email])
             wcs.writerow(["pwd",pwd])
-            wcs.writerow(["repwd",repwd])
+            # wcs.writerow(["repwd",repwd])
             wcs.writerow(["age",age])
         lst=Account()
         lst.aname=name
@@ -134,13 +150,21 @@ def seeschedule(request):
     global x
     global train
     global tno
+    global pnrlist
+    global pnr
     tno=''
     if request.method=="POST":
         if x:
             for i in request.POST:
                 if request.POST[i]=='Book':
                     tno=i
-
+            pnr=random.randint(1000000000,9999999999)
+            while True:
+                if pnr not in pnrlist:
+                    pnrlist+=[pnr]
+                    break
+            else:
+                pnr=str(random.randint(1000000000,9999999999))
             return HttpResponseRedirect('../form')
         else:
             return HttpResponseRedirect('../login')
@@ -157,36 +181,30 @@ def seeform(request):
     global train
     global pnrlist
     global tno
-    global day1
+    global fdate
+    global pnr
     if request.method=="POST":
         psgname=request.POST['name']
         age=request.POST['age']
         gender=request.POST['gender']
         quota=request.POST['quota']
-        pnr=random.randint(1000000000,9999999999)
-        while True:
-            if pnr not in pnrlist:
-                pnrlist+=[pnr]
-                break
-            else:
-                pnr=str(random.randint(1000000000,9999999999))
         for i in train:
             if i[0]==tno:
                 trtup=i
         tno=''
         seat=random.randint(1,50)
-        day=day1
-        day1=''
-        user=x[0].aname
-        
+        date=fdate
+        fdate=''
+        user=x[1]
+        print(date,'hi')
         curs.execute("insert into bookticket_passenger (Passenger_name,Gender,Age) values ('{}','{}',{})".format(psgname,gender,age))
         curs.execute("select passenger_id from bookticket_passenger")
         pid=curs.fetchall()[-1][0]
         print(pid)
-        curs.execute("insert into bookticket_journey (PNR_No,Train_No,Seat_No,Date,Time,Booked_user,Passenger_id,Quota)values('{}','{}',{},'{}','{}','{}',{},'{}')".format(pnr,trtup[0],seat,day,trtup[4],user,pid,quota))
+        curs.execute("insert into bookticket_journey (PNR_No,Train_No,Seat_No,Date,Time,Booked_user,Passenger_id,Quota)values('{}','{}',{},'{}','{}','{}',{},'{}')".format(pnr,trtup[0],seat,date,trtup[4],user,pid,quota))
         con.commit()
         time.sleep(3)
         return render(request,'Home_Page.html',{'al':x})
     else:
-        return render(request,"Passenger Details.html",{'al':x})
+        return render(request,"Passenger Details.html",{'al':x,'pnr':pnr})
     
